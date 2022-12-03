6df23d21bd9fd637de5db8e4aa57d0f493116edb
Stephen DeLorme <stephen@d.elor.me>
2022-12-03 13:19:52 -0500

    Added bitcoin icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import {BitcoinIcon} from '@bitcoin-design/bitcoin-icons-react/filled'
+
 // pleb.fm 
 // "Find your jukebox" screen?
 export default function Index() {
-  return <h1 className="text-4xl font-bold">Hello World! Pleb.FM</h1>
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Hello World! Pleb.FM</h1>
+      <BitcoinIcon className="w-4 h-4" />
+    </>
+  )
 }
