3e6bf5b9c5d8092502f34fe2133bf1bd55048cea
bnonni <me@bryanwnonni.com>
2022-12-04 16:22:26 -0500

    merge

--- pages/api/customers/[customerName].ts
+++ pages/api/customers/[customerName].ts
@@ -11,19 +11,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).json({success: false, error: 'Customer not found.'});
 
     return res.status(200).json({success: true, customer: customer});
-
-  // Adds new customer
-  } else if (req.method === 'POST') {
-    const { customerName, shortName, customerId } = req.body;
-    if (!customerName) res.status(400).json({error: `customerName must be present`});
-    const customer: Customer = {
-      customerName: customerName,
-      shortName: shortName,
-      id: customerId,
-      spotifyRefreshToken: ""
-    }
-    const result = await Customers.create(customer);
-    res.status(200).json({success: true, customer: result});
   }
 };
 
