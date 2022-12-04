f90b69e1d881766b3430a124180f7716eb13230e
Jordan Bravo <62706808+jordan-bravo@users.noreply.github.com>
2022-12-04 15:34:53 -0500

    Add User model & api route

--- models/User.ts
+++ models/User.ts
@@ -1,11 +1,48 @@
 import mongoose, { Schema } from 'mongoose';
 
+export enum Colors {
+    PurpleLight = 'purpleLight',
+    Purple = 'purple',
+    PurpleDark = 'purpleDark',
+    OrangeLight = 'orangeLight',
+    Orange = 'orange',
+    OrangeDark = 'orangeDark',
+    TealLight = 'tealLight',
+    Teal = 'teal',
+    TealDark = 'tealDark',
+}
+
+export enum Adjectives {
+    Fluffy = 'Fluffy',
+    Based = 'Based',
+    Insidious = 'Insidious',
+    Zazzy = 'Zazzy',
+    Silent = 'Silent',
+    Squealing = 'Squealing',
+    Wonderful = 'Wonderful',
+    Helpful = 'Helpful',
+}
+
+export enum Characters {
+    Honeybadger = 'Honeybadger',
+    Kitty = 'Kitty',
+    Fawkes = 'Fawkes',
+    Bankasaurus = 'Bankasaurus',
+    Goldbug = 'Goldbug',
+    Chaditha = 'Chaditha',
+}
+
 export type User = {
     userId: string;
     firstNym: string;
     lastNym: string;
-    refreshToken: string;
     avatar: string;
+};
+
+export const randomEnumValue = (enumeration: any) => {
+    const values = Object.keys(enumeration);
+    const enumKey = values[Math.floor(Math.random() * values.length)];
+    return enumeration[enumKey];
 };
 
 const UserSchema = new Schema<User>({
@@ -16,22 +53,14 @@ const UserSchema = new Schema<User>({
     },
     firstNym: {
         type: String,
-        unique: true,
         required: true,
     },
     lastNym: {
         type: String,
-        unique: true,
-        required: true,
-    },
-    refreshToken: {
-        type: String,
-        unique: true,
         required: true,
     },
     avatar: {
         type: String,
-        unique: true,
         required: true,
     },
 });
