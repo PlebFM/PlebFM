7a520f829e08d750c81d7fbcc7f5ca87def4a56c
bnonni <me@bryanwnonni.com>
2022-12-04 20:10:55 -0500

    /api/bidding/submit complete

--- pages/api/bidding/submit.ts
+++ pages/api/bidding/submit.ts
@@ -7,23 +7,24 @@ import { Bid } from "../../../models/Bid";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { customerName, userId, rHash, songId, bidAmount } = req.body;
-    console.log(req.body)
+    const now = Date.now().toString();
+    const newBid = {
+      userId: userId,
+      bidAmount: bidAmount,
+      timestamp: now,
+      rHash: rHash,
+    }
 
     const customer = await Customers.findOne({ shortName: customerName })
     if (!customer) return res.status(400).json({ success: false, error: 'Customer not found.' });
-    console.log("customer", customer)
+
     const user = await Users.findOne({ userId: userId })
     if (!user) return res.status(400).json({ success: false, error: 'User not found.' });
-    console.log("user")
-    const instances = await Instances.find()
-    if (!instances) return res.status(400).json({ success: false, error: 'Fatal: Instances not found.' });
-    console.log("instances")
-    const matchingBids = instances.filter((i: Instance) => i.Bids.filter((b: Bid) => b.rHash === rHash));
-    const bidIsUnique = matchingBids.length === 0;
-    if (!bidIsUnique) return res.status(400).json({ success: false, error: 'Bid rHash not unique.' });
 
-    const now = Date.now().toString();
-    const songInstance = await Instances.findOne({ songId: songId });
+    const matchingInstances = await Instances.findOne({ "Bids.rHash": { $eq: rHash } })
+    if (matchingInstances) return res.status(400).json({ success: false, error: 'Duplicate bid found.' });
+
+    const songInstance = await Instances.findOneAndUpdate({ songId: songId }, { $push: { Bids: newBid } });
     if (songInstance) return res.status(200).json({ success: true, new: false, instance: songInstance });
 
     const newInstance = await Instances.create({
@@ -32,15 +33,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       status: "queued",
       queueTimestamp: now,
       playedTimestamp: undefined,
-      Bids: new Array<Bid>({
-        userId: userId,
-        bidAmount: bidAmount,
-        timestamp: now,
-        rHash: rHash,
-      }),
+      Bids: new Array<Bid>(newBid),
     })
 
     return res.status(200).json({ success: true, new: true, instance: newInstance });
+
   } catch (error) {
     return res.status(400).json({ success: false, error: error });
   }
