a6a357e521ebb2629d4d59dd5d445f3f189fac23
Alex Lewin <alex.lewin@blockspaces.com>
2022-12-06 14:06:47 -0500

    submit bid works

--- pages/api/bidding/submit.ts
+++ pages/api/bidding/submit.ts
@@ -6,42 +6,48 @@ import { Bid } from "../../../models/Bid";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { customerName, userId, rHash, songId, bidAmount } = req.body;
-    const now: string = Date.now().toString();
-    const newBid: Bid = {
-      userId: userId,
-      bidAmount: bidAmount,
-      timestamp: now,
-      rHash: rHash,
+    if (req.method === 'POST') {
+      console.error(req.body);
+      const { customerName, userId, rHash, songId, bidAmount } = req.body;
+      const now: string = Date.now().toString();
+      const newBid: Bid = {
+        userId: userId,
+        bidAmount: bidAmount,
+        timestamp: now,
+        rHash: rHash,
+      }
+      console.error(req.body)
+
+      const customer = await Customers.findOne({ shortName: customerName });
+      if (!customer) return res.status(400).json({ success: false, error: `Customer not found.` });
+      console.error(customer)
+
+      // const user = await Users.findOne({ userId: userId });
+      // if (!user) return res.status(400).json({ success: false, error: 'User not found.' });
+      // console.error(user)
+
+      const matchingInstances = await Instances.findOne({ "Bids.rHash": { $eq: rHash } })
+      if (matchingInstances) return res.status(400).json({ success: false, error: 'Duplicate bid found.' });
+      console.error(matchingInstances)
+
+      const instance = await Instances.findOneAndUpdate({ songId: songId }, { $push: { Bids: newBid }, $inc: { runningTotal: bidAmount } }).catch(_ => {});
+      if (instance) return res.status(200).json({ success: true, new: false, instance: instance });
+
+      const runningTotal = (instance?.Bids?.reduce((p: any, c: any) => p.bidAmount + c.bidAmount).catch(_ => {}) ?? 0 + bidAmount
+      const newInstance: Instance = await Instances.create({
+        customerId: customer.id,
+        songId: songId,
+        status: "queued",
+        queueTimestamp: now,
+        Bids: new Array<Bid>(newBid),
+        runningTotal: runningTotal
+      })
+
+      return res.status(200).json({ success: true, new: true, instance: newInstance });
     }
 
-    const customer = await Customers.findOne({ shortName: customerName })
-    if (!customer) return res.status(400).json({ success: false, error: 'Customer not found.' });
-
-    const user = await Users.findOne({ userId: userId })
-    if (!user) return res.status(400).json({ success: false, error: 'User not found.' });
-
-    const matchingInstances = await Instances.findOne({ "Bids.rHash": { $eq: rHash } })
-    if (matchingInstances) return res.status(400).json({ success: false, error: 'Duplicate bid found.' });
-
-    const instance = await Instances.findOneAndUpdate({ songId: songId }, { $push: { Bids: newBid }, $inc: { runningTotal: bidAmount } })
-    if (instance) return res.status(200).json({ success: true, new: false, instance: instance });
-
-    const runningTotal = instance.Bids.reduce((p: any, c: any) => p.bidAmount + c.bidAmount) + bidAmount
-    const newInstance: Instance = await Instances.create({
-      customerId: customer.id,
-      songId: songId,
-      status: "queued",
-      queueTimestamp: now,
-      playedTimestamp: undefined,
-      Bids: new Array<Bid>(newBid),
-      runningTotal: runningTotal
-    })
-
-    return res.status(200).json({ success: true, new: true, instance: newInstance });
-
   } catch (error) {
-    return res.status(400).json({ success: false, error: error });
+    return res.status(400).json({ success: false, error: error.message });
   }
 }
 
