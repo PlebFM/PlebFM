9ecf7906d2829a8e3acf77e1c1e1d5fb3ea82642
Alex Lewin <alex.lewin@blockspaces.com>
2022-12-31 22:57:29 -0500

    moved app dir to pages

---
+++ pages/users/profile/dummyData.js
@@ -0,0 +1,164 @@
+export const dummyDataQueue = [
+    {
+      trackTitle: "Freedom",
+      artistName: "Rage Against the Machine",
+      feeRate: 100,
+      playing: false,
+      upNext: true,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+    {
+      trackTitle: "Roll Me Up",
+      artistName: "Willie Nelson, Snoop Dogg, Kris Kristofferson, Jamey Johnson",
+      feeRate: 98,
+      upNext: false,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+  ]
+
+export const dummyDataPlayed = [
+    {
+      trackTitle: "Happiness in Slavery",
+      artistName: "Nine Inch Nails",
+      feeRate: 100,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+    {
+      trackTitle: "Hips Don’t Lie (feat. Wyclef Jean)",
+      artistName: "Shakira",
+      feeRate: 98,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+    {
+      trackTitle: "Legend Has It",
+      artistName: "Run the Jewels",
+      feeRate: 87,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+    {
+      trackTitle: "Friday I'm in Love",
+      artistName: "The Cure",
+      feeRate: 76,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+    {
+      trackTitle: "Enter Sandman",
+      artistName: "Metallica",
+      feeRate: 65,
+      myPick: true,
+      bidders: [
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Zazzy",
+          lastNym: "Fawkes",
+          color: "orange"
+        },
+        {
+          firstNym: "Squealing",
+          lastNym: "Kitty",
+          color: "purpleLight"
+        },
+        {
+          firstNym: "Silent",
+          lastNym: "Bankasaurus",
+          color: "purpleDark"
+        },
+        {
+          firstNym: "Insidious",
+          lastNym: "Goldbug",
+          color: "tealLight"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        },
+        {
+          firstNym: "Fluffy",
+          lastNym: "Honeybadger",
+          color: "teal"
+        }
+      ]
+    },
+  ]
