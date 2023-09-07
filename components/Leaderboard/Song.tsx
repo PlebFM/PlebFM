9a331613533f362e0e1131f9d01a471568281e73
Stephen DeLorme <stephen@d.elor.me>
2023-09-07 14:55:47 -0400

    using SongQueue UI component in leaderbord

--- components/Leaderboard/Song.tsx
+++ components/Leaderboard/Song.tsx
@@ -3,6 +3,7 @@ import { SongObject } from '../../pages/[slug]/queue';
 import Avatar from '../Utils/Avatar';
 import Tag from '../Utils/Tag';
 import { User } from '../../models/User';
+import QueueSong from '../QueueSong';
 
 const cleanBidders = (bidders: User[]): User[] => {
   const seen = new Map<string, User>();
@@ -13,54 +14,8 @@ const cleanBidders = (bidders: User[]): User[] => {
 };
 export const Song = ({ song }: { song: SongObject }) => {
   return (
-    <div className="p-6 border-b border-white/20 w-full">
-      <div className="w-full flex justify-between space-x-4 w-full">
-        <div className="flex flex-col space-y-2">
-          {!song ? (
-            '...'
-          ) : (
-            <>
-              <div>
-                <p>{song.trackTitle}</p>
-                <p className="font-bold">{song.artistName}</p>
-              </div>
-              <div className="flex -space-x-1 items-center">
-                {cleanBidders(song.bidders)
-                  .slice(0, 5)
-                  .map((bidder, key) => (
-                    <div className="w-8" key={key}>
-                      <Avatar
-                        firstNym={bidder.firstNym}
-                        lastNym={bidder.lastNym}
-                        color={bidder.color}
-                        size="xs"
-                      />
-                    </div>
-                  ))}
-                {cleanBidders(song.bidders).length > 5 ? (
-                  <div className="pl-4 font-semibold text-lg">
-                    +{song.bidders.length - 5}
-                  </div>
-                ) : (
-                  ``
-                )}
-              </div>
-            </>
-          )}
-        </div>
-        {!song ? (
-          <></>
-        ) : song.upNext ? (
-          <Tag song={song} />
-        ) : (
-          <div>
-            <p className="font-normal text-6xl text-center">
-              {song.feeRate.toFixed(0)}
-            </p>
-            <p className="font-bold text-xs text-center"> sats / min</p>
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <QueueSong song={song} />
+    </>
   );
 };
