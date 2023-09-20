99c033350b29302f3a79aea1e544dc3e202e7b81
Alex Lewin <alexlwn123@gmail.com>
2023-09-20 18:43:29 -0400

    seo

--- components/Utils/Layout.tsx
+++ components/Utils/Layout.tsx
@@ -13,6 +13,10 @@ export default function Layout(props: LayoutProps) {
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Bitcoin-Powered Jukebox - An Ongoing Auction for the Next Song"
+        />
         <link rel="icon" href="/pleb-fm-favicon.svg" />
       </Head>
       <main className="m-auto fixed-w bg-gradient-to-b from-pfm-purple-400 to-pfm-purple-100">
